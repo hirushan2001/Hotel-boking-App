6c7182c116a394a1b7647d1ed525aef1bec230e7
hirushan2001 <135137760+hirushan2001@users.noreply.github.com>
2025-01-01 08:03:13 +0530

    update

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { Redirect } from "expo-router";
-const Home = () => {
+const HomeLayout = () => {
     return <Redirect href={"/(auth)/login"} />;
 };
 
-export default Home;
+export default HomeLayout;
